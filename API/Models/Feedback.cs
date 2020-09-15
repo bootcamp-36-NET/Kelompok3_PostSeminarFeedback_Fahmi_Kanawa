2bee094f1269ddc13bf18eb27e12a1a90b5a9166
fhmanwar <fhmianwar@gmail.com>
2020-09-15 20:21:14 +0700

    add model Feedback, Question, Training, TypeTraining

---
+++ API/Models/Feedback.cs
@@ -0,0 +1,34 @@
+﻿using API.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    [Table("TB_Trans_Feedback")]
+    public class Feedback : BaseModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Review { get; set; }
+        public float Rate { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+
+        [ForeignKey("Question")]
+        public int QuestionId { get; set; }
+
+        public DateTimeOffset CreateData { get; set; }
+        public DateTimeOffset UpdateDate { get; set; }
+        public DateTimeOffset DeleteData { get; set; }
+        public bool isDelete { get; set; }
+
+        public User User { get; set; }
+        public Question Question { get; set; }
+
+    }
+}
---
+++ API/Models/Training.cs
@@ -0,0 +1,35 @@
+﻿using API.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    [Table("TB_M_Training")]
+    public class Training : BaseModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Target { get; set; }
+        public string Location { get; set; }
+        public DateTimeOffset Schedule { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        [ForeignKey("Type")]
+        public int TypeId { get; set; }
+
+        public DateTimeOffset CreateData { get; set; }
+        public DateTimeOffset UpdateDate { get; set; }
+        public DateTimeOffset DeleteData { get; set; }
+        public bool isDelete { get; set; }
+
+        public User User { get; set; }
+        public TypeTraining Type { get; set; }
+
+    }
+}
