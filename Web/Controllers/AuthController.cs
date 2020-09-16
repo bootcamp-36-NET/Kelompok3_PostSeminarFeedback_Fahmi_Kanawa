46dc597cb33597d461bd85b34a6260387a76baea
KanawaA <67354503+KanawaA@users.noreply.github.com>
2020-09-16 22:17:46 +0700

    Create Web Service (Modul User)

--- Web/Controllers/AuthController.cs
+++ Web/Controllers/AuthController.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using API.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Web.Controllers
 {
     public class AuthController : Controller
     {
+        readonly HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:53564/api/")
+        };
+
         [Route("login")]
         public IActionResult Login()
         {
@@ -20,6 +31,13 @@ namespace Web.Controllers
             return View();
         }
 
+        [Route("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("/login");
+        }
+
         [Route("verify")]
         public IActionResult Verify()
         {
@@ -30,6 +48,87 @@ namespace Web.Controllers
         public IActionResult Notfound()
         {
             return View();
+        }
+
+        [Route("validate")]
+        public IActionResult Validate(UserVM userVM)
+        {
+            var json = JsonConvert.SerializeObject(userVM);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(json);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            if (userVM.Name == null)
+            { // Login
+                HttpResponseMessage result = null;
+                if (userVM.VerifyCode != null)
+                {
+                    result = client.PostAsync("users/code/", byteContent).Result;
+                }
+                else if (userVM.VerifyCode == null)
+                {
+                    result = client.PostAsync("users/login/", byteContent).Result;
+                }
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var data = result.Content.ReadAsStringAsync().Result;
+                    if (data != null)
+                    {
+                        HttpContext.Session.SetString("token", "Bearer " + data);
+                        var handler = new JwtSecurityTokenHandler();
+                        var tokenS = handler.ReadJwtToken(data);
+                        var jwtPayloadSer = JsonConvert.SerializeObject(tokenS.Payload.ToDictionary(x => x.Key, x => x.Value));
+                        var jwtPayloadDes = JsonConvert.DeserializeObject(jwtPayloadSer).ToString();
+                        var account = JsonConvert.DeserializeObject<UserVM>(jwtPayloadSer);
+
+                        if (!account.VerifyCode.Equals(""))
+                        {
+                            return Json(new { status = true, msg = "VerifyCode" });
+                        }
+                        else if (account.RoleName != null)
+                        {
+                            HttpContext.Session.SetString("id", account.Id);
+                            HttpContext.Session.SetString("uname", account.Name);
+                            HttpContext.Session.SetString("email", account.Email);
+                            HttpContext.Session.SetString("lvl", account.RoleName);
+                            if (account.RoleName == "Admin")
+                            {
+                                return Json(new { status = true, msg = "Login Successfully !" });
+                                //return View("~/Views/Auth/verify.cshtml");
+                            }
+                            return Json(new { status = true, msg = "Login Successfully !" });
+                        }
+                        return Json(new { status = false, msg = "You Don't Have Permissions! Please Contact Administrator" });
+                    }
+                    return Json(new { status = false, msg = result.Content.ReadAsStringAsync().Result });
+                }
+                return Json(new { status = false, msg = result.Content.ReadAsStringAsync().Result });
+            }
+            else if (userVM.Name != null)
+            { // Register
+                var result = client.PostAsync("users/", byteContent).Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return Json(new { status = true, code = result, msg = "Register Success! " });
+                }
+                return Json(new { status = false, msg = result.Content.ReadAsStringAsync().Result });
+            }
+            return Redirect("/login");
+        }
+
+        [Route("getjwt")]
+        public IActionResult GetName()
+        {
+            var stream = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImRiM2VhZmIxLTkyMWUtNDdmYS1hOGFiLTIwNDYxMzkxM2FlMCIsIlVzZXJuYW1lIjoiUmlmcXkiLCJFbWFpbCI6Im11aGFtbWFkcmlmcWkwQGdtYWlsLmNvbSIsIlJvbGVOYW1lIjoiU2FsZXMiLCJleHAiOjE1OTk1NDY0MTYsImlzcyI6IkludmVudG9yeUF1dGhlbnRpY2F0aW9uU2VydmVyIiwiYXVkIjoiSW52ZW50b3J5c2VydmljZVBvc3RtYW50Q2xpZW50In0.ziIjgvqJdH17w4HwHGzvXyZTUz41S06i0xHWGxAnY2M";
+            var handler = new JwtSecurityTokenHandler();
+            var tokenS = handler.ReadJwtToken(stream);
+
+            var jwtPayloadSer = JsonConvert.SerializeObject(tokenS.Payload.ToDictionary(x => x.Key, x => x.Value));
+            var jwtPayloadDes = JsonConvert.DeserializeObject(jwtPayloadSer).ToString();
+            var account = JsonConvert.DeserializeObject<UserVM>(jwtPayloadSer);
+
+            // Output the whole thing to pretty Json object formatted.
+            return Json(new { account.Id, account.Name, account.Email, account.RoleName, account.VerifyCode });
         }
     }
 }
--- Web/Controllers/DashboardController.cs
+++ Web/Controllers/DashboardController.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
     public class DashboardController : Controller
     {
+        readonly HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:53564/api/")
+        };
+
         public IActionResult Index()
+        {
+            if (HttpContext.Session.IsAvailable)
+            {
+                if (HttpContext.Session.GetString("lvl") == "Admin")
+                {
+                    return View();
+                }
+                return Redirect("/profile");
+            }
+            return RedirectToAction("Login", "Auth");
+        }
+
+        [Route("profile")]
+        public IActionResult Profile()
         {
             return View();
         }
